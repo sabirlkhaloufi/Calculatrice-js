53442747a8d0d9afabcbc6dabbabd0eb8805e639
sabir lkhaloufi <83829858+sabirlkhaloufi@users.noreply.github.com>
2022-03-09 07:20:34 +0000

    add js a calcul

--- js/main.js
+++ js/main.js
@@ -1,19 +1,87 @@
-let nbrBtn = document.querySelectorAll(".nbr");
-let viewNbr1 = document.querySelector(".nbr1");
-let viewNbr2 = document.querySelector(".nbr2");
-let op = document.querySelector(".op");
-console.log(op);
-let nbr1 = "";
-let nbr2 = "";
-for(let i =0; i<nbrBtn.length;i++){
-    nbrBtn[i].addEventListener("click",function(){
-        if(op = *){
-            nbr2 = nbr2 + nbrBtn[i].innerHTML;
-            viewNbr1.innerHTML = nbr2;
+let nbr = document.querySelectorAll('.nbr');
+let operation = document.querySelectorAll('.operat');
+let result = document.querySelector('.btngale');
+let nbr1 = document.querySelector('.nbr1');
+let op = document.querySelector('.op');
+let nbr2 = document.querySelector('.nbr2');
+let affRes = document.querySelector('.affichage');
+let delet = document.querySelector('.delete');
+let res = 0;
+
+
+function somme(nbr1,nbr2){
+    return nbr1 + nbr2;
+}
+function addition(nbr1,nbr2){
+    return nbr1 / nbr2;
+}
+function multiplication(nbr1,nbr2){
+    return nbr1 * nbr2;
+}
+function sousTraction(nbr1,nbr2){
+    return nbr1 - nbr2;
+}
+
+function operate(nbr1,nbr2){
+    if(op.innerHTML == '*'){
+        res = multiplication(nbr1,nbr2);
+        affRes.innerHTML = res;
+    }
+    else if(op.innerHTML == '+'){
+        res = somme(nbr1,nbr2);
+        affRes.innerHTML = res;
+    }
+    else if(op.innerHTML == '/'){
+        res = addition(nbr1,nbr2);
+        affRes.innerHTML = res;
+    }
+    else if(op.innerHTML == '-'){
+        res = sousTraction(nbr1,nbr2);
+        affRes.innerHTML = res;
+    }
+}
+
+// calcul operation
+result.addEventListener("click",function(){
+    operate(Number(nbr1.innerHTML),Number(nbr2.innerHTML));
+});
+
+// click numbers
+nbr.forEach(value => {
+    value.addEventListener("click",function(){
+        if(op.innerHTML == '*' || op.innerHTML == '-' || op.innerHTML == '+' || op.innerHTML == '/'){
+            nbr2.innerHTML += value.innerHTML;
         }
         else{
-            nbr1 = nbr1 + nbrBtn[i].innerHTML;
-        viewNbr1.innerHTML = nbr1;
+            if(nbr1.innerHTML == '0'){
+                nbr1.innerHTML = "";
+            }
+            else{
+                nbr1.innerHTML += value.innerHTML;
+            }
         }
     })
-}
+});
+
+//click operation 
+operation.forEach(value => {
+    value.addEventListener("click",function(){
+            op.innerHTML = value.innerHTML;
+    })
+});
+// null all content
+document.querySelector('.vider').addEventListener("click",function(){
+    document.querySelector('.nbr1').innerHTML = "0";
+    document.querySelector('.nbr2').innerHTML = "";
+    document.querySelector('.op').innerHTML = "";
+})
+
+delet.addEventListener("click",function(){
+    let array = Array.from(nbr1.innerHTML);
+    console.log(array);
+    for(let i = array.length-1;i>0;i--){
+        array = array - array[array.length];
+        break;
+    }
+    console.log(array);
+})
